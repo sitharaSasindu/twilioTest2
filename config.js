c9caab80f1a581fabf1315477a4db38cf2be7b3b
sitharaSasindu <sasindusithara@gmail.com>
2018-07-18 10:41:52 +0530

    a

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: '46155612',
+  apiSecret: 'd41000634278874b551d481445ee2f1a9ddd5098',
+  sip: {
+    uri: 'tokboxbreakout.pstn.twilio.com',
+    username: 'auth',
+    password: 'psiTwilio#12',
+  },
+  callerId: '+18435942018',
+};
